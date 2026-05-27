c7a5c7224254557fe0b3e9b7cb3d5d57bdd8b0e9
Jean <jgpa16@gmail.com>
2026-05-27 18:51:38 -0300

    adiciona cors

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# LIBERA ACESSO DO BASE44
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
